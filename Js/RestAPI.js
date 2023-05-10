d85c2f42c798dd23d90f09bbc66bd651cb0812ce
Vadim Dovnar <vadim.dovnar.93@gmail.com>
2023-05-10 21:44:09 +0300

    create method to deploy deal with person

--- Js/RestAPI.js
+++ Js/RestAPI.js
@@ -1,36 +1,62 @@
-// export class RestAPI {
-//     constructor() {
 
-//     }
+export default class RestAPI {
+    constructor() {
 
-//     createDeal() {
-//         const api_token = '1408ff17c6fdd6e5eb5a28e361027e7dfadc81a9';
-//         const url = 'https://api.pipedrive.com/v1/deals';
-
-//         const deal_data = {
-//             title: 'New deal2',
-//             value: 1000,
-//             currency: 'USD',
-//             person_id: 1234,
-//             org_id: 5678
-//         };
-        
-//         fetch(`${url}?api_token=${api_token}`, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(deal_data)
-//         })
-//         .then(response => response.json())
-//         .then(data => console.log(data))
-//         .catch(error => console.error(error));
-//     }
+    }
     
-// }
-
-export function set(e) {
-    e.preventDefault();
-    alert('Setting HIIIII');
+    createDealWithPerson(event) {
+        event.preventDefault();
+        const API_TOKEN = '2217a7e1b08b421cb2d9237dc8094556ae74610a';
+        const API_BASE_URL = 'https://api.pipedrive.com/v1';
+        const createPerson = async (name, email) => {
+            try {
+              const response = await fetch(`${API_BASE_URL}/persons?api_token=${API_TOKEN}`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  api_token: API_TOKEN,
+                  name,
+                  email,
+                }),
+              });
+              const data = await response.json();
+              return data.data.id;
+            } catch (error) {
+              console.error(error);
+            }
+          };
+          
+          // create a deal
+          const createDeal = async (personId, title, value) => {
+            try {
+              const response = await fetch(`${API_BASE_URL}/deals?api_token=${API_TOKEN}`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  api_token: API_TOKEN,
+                  person_id: personId,
+                  title,
+                  value,
+                }),
+              });
+              const data = await response.json();
+              return data.data.id;
+            } catch (error) {
+              console.error(error);
+            }
+          };
+          const main = async () => {
+            const personId = await createPerson('John Doe', 'john.doe@example.com');
+            const dealId = await createDeal(personId, 'Sample Deal', 1000);
+            console.log(`Created a deal with ID ${dealId}`);
+          };
+          
+          main();
+    }
+    
 }
 
--- Js/index.js
+++ Js/index.js
@@ -2,22 +2,11 @@
 //Client ID: 6fb7c5caffb35cbf
 //Client Secret: a22c5b991057b71be9efe8156498f088423e7294
 
-import  {set} from './RestAPI.js';
-//get submit button element
+//import REST class
+import  RestAPI from './RestAPI.js';
+const restAPI = new RestAPI();
+
+// add click event for the button
 const submitButton = document.querySelector('.items-block__submit');
-//add click event to submit button
-submitButton.addEventListener('click', set);
+submitButton.addEventListener('click', restAPI.createDealWithPerson);
 
-
-
-
-
-//get deal
-// fetch(`${url}?api_token=${api_token}`)
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error(error));
-
-//create deal  
-  
-  
