75f350ef6ae2d4257b4c326b6db90a823997ca6c
Vadim Dovnar <vadim.dovnar.93@gmail.com>
2023-05-11 05:16:36 +0300

    test commit

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,3 @@
-//API Token: 1408ff17c6fdd6e5eb5a28e361027e7dfadc81a9
-//Client ID: 6fb7c5caffb35cbf
-//Client Secret: a22c5b991057b71be9efe8156498f088423e7294
 
 //import REST class
 import  RestAPI from './RestAPI.js';
